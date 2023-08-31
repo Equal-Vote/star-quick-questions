6b2ce6d2d320172296d580afe97d6f1fbc2da191
Arend Peter Castelein <apc1993@gmail.com>
2023-08-31 11:14:44 -0700

    Add github pages

--- src/components/Instructions.js
+++ src/components/Instructions.js
@@ -13,15 +13,19 @@ const Instructions = ({allQuestions, hidden, setHidden}) => {
                     <br/><br/>
                     This to help activists polish their talking points when discussing STAR Voting.
                     <br/><br/>
-                    Here's how it works:
+                    <h3> How it works</h3>
                     <ol>
-                    <li> Select the difficulty you want </li>
+                    <li> Select the difficulty </li>
                     <li> Click "Next Question" for a randomly selected question </li>
-                    <li> Spend 1-2 minutes giving a answer the to question (ideal out loud) </li>
+                    <li> Spend 1-2 minutes giving a answer the to question (ideally out loud) </li>
                     <li> Use the links under the question for a sample answer and see how you did :) </li>
                     </ol>
-                    <br/>
-                    For suggesting questions, please use <a href="https://docs.google.com/forms/d/e/1FAIpQLSdAEplMSYQzQIKmv9csr70ca2D8YQAtgYHY62mxac7mdACdjg/viewform">this form</a>
+                    <h3> Links</h3>
+                    <ul>
+                        <li><a href="https://docs.google.com/spreadsheets/d/1dfUrbGTpP5ShpR8TQHYx1Aam1ENflmss-WtcfnDzvuE/edit?usp=sharing">Full List of questions</a></li>
+                        <li><a href="https://docs.google.com/forms/d/e/1FAIpQLSdAEplMSYQzQIKmv9csr70ca2D8YQAtgYHY62mxac7mdACdjg/viewform">Suggest more questions</a></li>
+                        <li><a href="starvoting.us/join">Join our community of volunteers!</a></li>
+                    </ul>
                 </p>
             </div>
             <button onClick={() => setHidden(h => !h)}>{hidden? '?' : '<'}</button>
